d2e8a4ce1028ab75ffa3269b1946bcfa311f812a
Juan Camilo Duque <kcamilito@gmail.com>
2024-11-04 18:47:04 -0500

    agrego libreria valibot al proyecto

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,8 @@
+
 import { SearchType } from "../types"
 import axios from "axios"
-import {z} from 'zod'
+import {object,number,string,parse,InferOutput} from 'valibot'
+//import {z} from 'zod'
 // import { Weather } from "../types"
 
 // type guards
@@ -16,16 +18,28 @@ import {z} from 'zod'
 // }
 
 // Zod
-const Weather = z.object({
-    name:z.string(),
-    main:z.object({
-        temp:z.number(), 
-        temp_max: z.number(),
-        temp_min: z.number(),
+//const Weather = z.object({
+//    name:z.string(),
+//    main:z.object({
+//        temp:z.number(), 
+//        temp_max: z.number(),
+//        temp_min: z.number(),
+//    })
+//})
+
+//type Weather = z.infer<typeof Weather>
+
+// valibot
+const schemaWeather = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number(),
     })
 })
 
-type Weather = z.infer<typeof Weather>
+type Weather = InferOutput<typeof schemaWeather>
 
 export default function useWeather() {
 
@@ -56,9 +70,14 @@ export default function useWeather() {
 
             // Zod
 
+            //const { data: weatherResult } = await axios(callCurrentWeather)
+            //const result = Weather.safeParse(weatherResult)
+
+            // Valibot
             const { data: weatherResult } = await axios(callCurrentWeather)
-            const result = Weather.safeParse(weatherResult)
+            const result = parse(schemaWeather,weatherResult)
             console.log(result)
+
 
         } catch (error) {
             console.log('Ocurrio algo inesperado:', error)
