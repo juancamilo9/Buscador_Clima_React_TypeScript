fec8efabc4501d2610f10a667aa127a7ebe77ee0
Juan Camilo Duque <kcamilito@gmail.com>
2024-11-04 18:16:21 -0500

    Agrego Zod al proyecto

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,16 +1,44 @@
 import { SearchType } from "../types"
 import axios from "axios"
+import {z} from 'zod'
+// import { Weather } from "../types"
+
+// type guards
+// const isWeatherResult = (weather: unknown): weather is Weather => {
+//     return(
+//         Boolean(weather) &&
+//         typeof weather === "object" && 
+//         typeof (weather as Weather).name === "string" &&
+//         typeof (weather as Weather).main.temp === "number" &&
+//         typeof (weather as Weather).main.temp_max === "number" &&
+//         typeof (weather as Weather).main.temp_min === "number"
+//     )
+// }
+
+// Zod
+const Weather = z.object({
+    name:z.string(),
+    main:z.object({
+        temp:z.number(), 
+        temp_max: z.number(),
+        temp_min: z.number(),
+    })
+})
+
+type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
-    
-    const fetchWeather=async (search:SearchType) =>{
+
+
+
+    const fetchWeather = async (search: SearchType) => {
         const appId = import.meta.env.VITE_APPI_KEY
         try {
             // generamos la url para un llamado a la api Geo
             const callGeoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
-            
+
             // consultamos a la api para obtener una respuesta
-            const {data} = await axios(callGeoUrl)
+            const { data } = await axios(callGeoUrl)
 
             // obtenemos la latitud y la longitud de la respesuta para poder llamar nuevametne a la api
             const lat = data[0].lat
@@ -19,14 +47,25 @@ export default function useWeather() {
             const callCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
             // reasignamos el valor de data para que no tenga problemas con el data antes creado
-            const {data:weatherResult} = await axios(callCurrentWeather)
-            console.log(weatherResult) 
+            // type guards
+            // const { data: weatherResult } = await axios(callCurrentWeather)
+            // const result = isWeatherResult(weatherResult)
+            // if(result){
+            //     weatherResult.name
+            // }
+
+            // Zod
+
+            const { data: weatherResult } = await axios(callCurrentWeather)
+            const result = Weather.safeParse(weatherResult)
+            console.log(result)
+
         } catch (error) {
-            console.log('Ocurrio algo inesperado:' ,error)
+            console.log('Ocurrio algo inesperado:', error)
         }
     }
-    
-    return{
+
+    return {
         fetchWeather
     }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,12 @@ export type CountriesType = {
     code:string
     name:string
 }
+
+export type Weather = {
+    name:string
+    main:{
+        temp:number 
+        temp_max: number
+        temp_min: number
+    }
+}
